c904b14ed98ffc2b54d22d02ed285f229322efc2
april <martinezapril56@yahoo.com>
2018-12-29 02:49:18 -0500

    forms

---
+++ form/form.php
@@ -0,0 +1,14 @@
+<<!DOCTYPE html>
+<html>
+<head>
+    
+</head>
+<body>
+    <?php
+
+    ?>
+    <form action ="form.php" method="post">
+    First Name: <input type="text" name="fname">
+    </form>
+</body>
+</html>
