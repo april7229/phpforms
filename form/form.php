8da8267c9d092faa3c4a51066c26db05592943e2
april <martinezapril56@yahoo.com>
2018-12-29 19:25:00 -0500

    form

--- form/form.php
+++ form/form.php
@@ -8,7 +8,11 @@
 
     ?>
     <form action ="form.php" method="post">
-    First Name: <input type="text" name="fname">
+    First Name: <input type="text" name="fname"><br />
+    Last Name : <input type="text" name="lname"><br />
+    About: <textarea name="about"></textarea><br />
+    <input type="reset" name="reset">
+    <input type="submit" name="submit" value="Go">
     </form>
 </body>
 </html>
