8c3d5a4cb8dd5640adab779a4975b71cefebc389
april <martinezapril56@yahoo.com>
2018-12-30 19:47:26 -0500

    email

--- form/contact.php
+++ form/contact.php
@@ -16,6 +16,7 @@
     <input type="text" name="mail" placeholder="Your e-mail">
     <input type="text" name="subject" placeholder="Subject">
     <textarea name="message" placeholder="Message"></textarea>
+    <button type="submit" name="submit">SEND MAIL</button>
     </form>
 
     </main>
