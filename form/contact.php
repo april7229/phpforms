bf3f380e390aa2ccb8278afafba0163f63c9c206
april <martinezapril56@yahoo.com>
2018-12-30 19:43:58 -0500

    email

---
+++ form/contact.php
@@ -0,0 +1,23 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <main>
+    <p>SEND E-MAIL</p>
+    <form class="Contact-form" action="contact.php" method="post">
+    <input type="text" name="name" placeholder="Full name">
+    <input type="text" name="mail" placeholder="Your e-mail">
+    <input type="text" name="subject" placeholder="Subject">
+    <textarea name="message" placeholder="Message"></textarea>
+    </form>
+
+    </main>
+</body>
+</html>
